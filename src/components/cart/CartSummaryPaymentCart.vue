5f2db08b99002dd0cd1b9cd0bbe54ff297415f79
chrystik <chrystik21@gmail.com>
2022-01-28 17:31:26 +0300

    init

---
+++ src/components/cart/CartSummaryPaymentCart.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="cart-item-card">
+        <h3>Cart Total: ${{ cart_total.toFixed(2) }}</h3>
+
+        <button class="view-product-button">Pay by Credit Card</button>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        cart_total() {
+            return this.$store.getters.cartTotal
+        }
+    }
+}
+</script>
+
+<style scoped>
+    button.view-product-button {
+        padding: 10px;
+        background-color: rgb(79, 160, 187);
+        border: none;
+        color: white;
+        font-weight: bold;
+        font-size: 1.15rem;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+</style>
---
+++ src/data/items.js
@@ -0,0 +1,39 @@
+const items = [
+    {
+        id: 1,
+        name: "Comfy Slippers",
+        price: 12.00,
+        category: 'Footwear',
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+    },
+    {
+        id: 2,
+        name: "Trail Runners",
+        price: 45.50,
+        category: 'Footwear',
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+    },
+    {
+        id: 3,
+        name: "Hikers",
+        price: 80.00,
+        category: 'Footwear',
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+    },
+    {
+        id: 4,
+        name: "Ski Jacket",
+        price: 120.00,
+        category: 'Jackets',
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+    },
+    {
+        id: 5,
+        name: "Winter Parka",
+        price: 230.50,
+        category: 'Jackets',
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+    }
+]
+
+export default items
